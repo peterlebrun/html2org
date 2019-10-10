26e8aae20a930b4212fc9591df2af99566a3a67e
peterlebrun <peterlebrun@gmail.com>
2019-10-10 18:00:14 -0400

    figured out a bit more now I'm tired

--- html2org.el
+++ html2org.el
@@ -1,6 +1,8 @@
 ; Provide utility to extract links from web pages
 ; And turn those into org-mode projects
 
+(require 'dom)
+
 (setq url "https://mitpress.mit.edu/sites/default/files/sicp/full-text/book/book-Z-H-4.html#%_toc_start")
 
 (defun h2o-extract-response-code ()
@@ -17,7 +19,10 @@
    ;; @TODO Handle error
    (when (and (equal response-code "200") (search-forward "\n\n" nil t))
      (let* ((doc-dom (libxml-parse-html-region (point) (point-max)))
-            (body (caddr (dom-children doc-dom))))
-       (debug (dom-children body))))))
+            (body (caddr (dom-children doc-dom)))
+            (div1 (elt body 4)))
+       ; TODO: Figure out how to get the p and then the a and loop through those
+       ; and grab the hrefs from those a's
+       (debug (dom-attr div1 'class))))))
 
 (url-retrieve url 'h2o-process-response)
