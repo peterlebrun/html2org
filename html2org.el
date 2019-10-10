477603133541f816861ed23671548b2c99f5de98
peterlebrun <peterlebrun@gmail.com>
2019-10-10 17:44:42 -0400

    starting to learn how dom-* functions work

--- html2org.el
+++ html2org.el
@@ -12,21 +12,12 @@
  "Extract the html response from the buffer returned by url-http."
  (set-buffer-multibyte t)
  (let ((response-code (h2o-extract-response-code)))
-   ;(debug response-code)
    (goto-char (point-min)) ;; Ensure that we start at beginning of buffer
-   ;(debug (point))
    ;; @TODO Handle other 200
    ;; @TODO Handle error
    (when (and (equal response-code "200") (search-forward "\n\n" nil t))
-     (let ((dom (libxml-parse-html-region (point) (point-max))))
-       (debug (dom-tag "html"))))))
-
-     ;(debug "foo"))))
-     ;(ewl-parse-json))))
-
-;(defun h2o-debug ()
-;  "Debug body of HTTP response"
-;  (let ((resp (h2o-process-response)))
-;    (debug resp)))
+     (let* ((doc-dom (libxml-parse-html-region (point) (point-max)))
+            (body (caddr (dom-children doc-dom))))
+       (debug (dom-children body))))))
 
 (url-retrieve url 'h2o-process-response)
