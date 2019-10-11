a5e9b3fd726b8bb0301467c96ceebea3be9d6706
peterlebrun <peterlebrun@gmail.com>
2019-10-11 17:21:16 -0400

    try new approach to identify anchor tags

--- html2org.el
+++ html2org.el
@@ -18,11 +18,31 @@
    ;; @TODO Handle other 200
    ;; @TODO Handle error
    (when (and (equal response-code "200") (search-forward "\n\n" nil t))
-     (let* ((doc-dom (libxml-parse-html-region (point) (point-max)))
-            (body (caddr (dom-children doc-dom)))
-            (div1 (elt body 4)))
+     (let* ((dom (buffer-substring (point) (point-max)))
+            (first-a-start (string-match "<a " dom))
+            (first-a-end (progn
+                           (string-match "</a>" dom)
+                           (match-end 0)))
+            (first-a (substring dom first-a-start first-a-end))
+            )
+       ;(debug first-a-start)
+       ;(debug first-a-end)
+       (debug first-a)))))
+
+     ;(search-forward "<body>" nil t)
+     ;(goto-char (- (point) 6))
+     ;(search-forward "<p>" nil t)
+     ;(goto-char (- (point) 3))
+     ;(search-forward "</p>"
+
+       ;(when (search-forward "<body" nil t))
+       ;(debug doc)
+       ;))))
+     ;(let* ((doc-dom (libxml-parse-html-region (point) (point-max)))
+     ;       (body (caddr (dom-children doc-dom)))
+     ;       (div1 (elt body 5)))
        ; TODO: Figure out how to get the p and then the a and loop through those
        ; and grab the hrefs from those a's
-       (debug (dom-attr div1 'class))))))
+       ;(debug (dom-children div1))))))
 
 (url-retrieve url 'h2o-process-response)
